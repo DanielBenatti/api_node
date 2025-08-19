3193bd2a03ef84ad4e134580a03c16391f95781f
DanielBenatti <daniellopesbenatti2@gmail.com>
2025-08-19 20:06:13 -0300

    aula15

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-import express from 'express'
+import express from 'express';
 
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
 const app = express()
 
 app.use(express.json())
 
-const usuarios = []
-
-
 //criar as rotas
 
-app.get("/cadastro",(req, res)=>{
-    //res.send('Ok')
+app.get("/cadastro",async(req, res)=>{
+    const usuarios = await prisma.usuario.findMany();
     res.status(200).json(usuarios)
 })
 
-app.post("/cadastro",(req,res)=>{
-    //console.log(req.body)
-    //res.send("OK 2.0")
-    usuarios.push(req.body)
+app.post("/cadastro",async(req,res)=>{
+    await prisma.usuario.create({
+        data:{
+            email: req.body.email,
+            nome: req.body.nome,
+            idade: req.body.idade,
+        }
+    })
     res.status(201).json(req.body)
 })
 
