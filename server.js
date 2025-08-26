937b0ba1929e35a6d0b50ec1303334d3885bfcf7
DanielBenatti <daniellopesbenatti2@gmail.com>
2025-08-26 11:45:51 -0300

    CONSERTADO

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-import express from 'express';
+import express from 'express'
 
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
-const app = express()
 
+const app = express()
 app.use(express.json())
 
 //criar as rotas
-
-app.get("/cadastro",async(req, res)=>{
-    const usuarios = await prisma.usuario.findMany();
-    res.status(200).json(usuarios)
-})
-
-app.post("/cadastro",async(req,res)=>{
+app.post('/cadastro', async (req,res)=>{
     await prisma.usuario.create({
         data:{
             email: req.body.email,
             nome: req.body.nome,
-            idade: req.body.idade,
+            idade: req.body.idade
         }
     })
+
     res.status(201).json(req.body)
 })
 
-//cibfugyrar porta do servidor
-app.listen(3000,()=>{console.log('Primal Ok')})
+app.get('/cadastro', async(req,res)=>{
+    const usuarios = await prisma.usuario.findMany();
+    //res.send('Funfou no get :)')
+    res.status(200).json(usuarios)
+})
+
+//configurar porta do servidor
+app.listen(3000,()=>{console.log('Servidor Rodando!')})
