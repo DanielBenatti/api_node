10f0e18b32cdb7e952a81c59eeef2c9df798cb32
DanielBenatti <daniellopesbenatti2@gmail.com>
2025-08-26 12:20:27 -0300

    FEITO

--- server.js
+++ server.js
@@ -26,5 +26,34 @@ app.get('/cadastro', async(req,res)=>{
     res.status(200).json(usuarios)
 })
 
+app.put('/cadastro/:id', async(req,res)=>{
+    //console.log(req.params.id)   
+    
+    await prisma.usuario.update({
+        where:{
+            id:req.params.id
+        },
+        data:{
+            email: req.body.email,
+            nome: req.body.nome,
+            idade: req.body.idade
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/cadastro/:id', async(req,res)=>{
+    //console.log(req.params.id)   
+    
+    await prisma.usuario.delete({
+        where:{
+            id:req.params.id
+        },
+    })
+    
+    res.status(201).json({"message":"Usuário Removido"})
+})
+
 //configurar porta do servidor
 app.listen(3000,()=>{console.log('Servidor Rodando!')})
